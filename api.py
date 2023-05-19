867d034327ada8a326c132fb29930954b7845448
Nico Pistor <nico.pistor@hs-furtwangen.de>
2023-05-19 13:13:32 +0200

    arguments and different connection strategies

--- api.py
+++ api.py
@@ -1,64 +1,87 @@
 from clearml.backend_api.session.client import APIClient
 from clearml.backend_api.session.client import StrictSession
+from clearml.backend_api.session import Session
 from utils import transfer_model, transfer_task
 from clearml import TaskTypes
 import sys
 import getopt
+import argparse
 
-# Args
+# **********************************************************************
+#                               Arguments
+# **********************************************************************
+
+# This is intended to be used be the LNU contact person.
+# Therefore, the LNU environment is probably the local installed version.
+# This connection is established by using the local client configuration
+# The connection to the other instance can be established by passing the
+# client credentials.
 
 api_conf_source = ''
-api_conf_target = ''
+api_target_host = ''
+api_target_api_key = ''
+api_target_secret_key = ''
+target_project_name = "LNU Project"
 
-try:
-    args, vals = getopt.getopt(
-        sys.argv[1:],
-        "hmo:",
-        ['source', 'target']
-    )
+parser = argparse.ArgumentParser()
+parser.add_argument('--source', type=str)
+parser.add_argument('--target-host', type=str)
+parser.add_argument('--target-api-key', type=str)
+parser.add_argument('--target-secret', type=str)
+parser.add_argument('--project', type=str)
 
-    for arg, val in args:
-        if arg in ('-s', '--source'):
-            api_conf_source = val
-        elif arg in ('-t', '--target'):
-            api_conf_target = val
-            
-except getopt.error as error:
-    print(error)
-    exit
+args = parser.parse_args()
 
-if len(sys.argv) < 2:
-    print("ERROR: not enough arguments passed")
-    exit
+api_conf_source = args.source
+api_target_host = args.target_host
+api_target_api_key = args.target_api_key
+api_target_secret_key = args.target_secret
+target_project_name = args.project
+
+# **********************************************************************
+#                         Task and Model IDs
+# **********************************************************************
+
+# Models which are connected to tasks do not need to be added to the list
 
 task_ids = [
-    '7f2a7105fcc743cb8f3f225f324e51bd',
-    'c5e89d9085ec4ec68f13c8430d25ce19',
-    'cf5a7e75f6754629a356f07704b7d106'
+    '5bd8f005e7c2444d95b4c2115b15598f',
+    '499994df52df4fe19e00c481ca948588',
+    'd4a17dc2ea8547ed9e78d4f4420e4241'
 ]
 
-model_ids = [
-    '731a44f3d5ce4a799cd67238bcea8b04'
-]
-
-target_project_name = "Transfer Testrr"
+model_ids = []
 
 # Constants and Variables
 transfer_tag_name = "LNU TRANSFER"
 task_map = dict()
 model_map = dict()
 
-# Setup connections
 
+# **********************************************************************
+#                           Setup Connections
+# **********************************************************************
+
+# Setup source connection
 session_source = StrictSession(config_file=api_conf_source)
-session_target = StrictSession(config_file=api_conf_target)
+
+# Setup target connection
+# Two strict session objects does not work 
+# An alternative connection pproach was required
+session_target = Session(
+    api_key=api_target_api_key,
+    secret_key=api_target_secret_key,
+    host=api_target_host
+)
 
 source = APIClient(session=session_source)
 target = APIClient(session=session_target)
 
+# **********************************************************************
+#                         Execute Transfer
+# **********************************************************************
 
-# Check if target project exists
-
+# Check if target project exists, otherwise create it
 project_list = target.projects.get_all(name=target_project_name)
 if len(project_list) > 0:
     target_project_id = project_list[0].id
